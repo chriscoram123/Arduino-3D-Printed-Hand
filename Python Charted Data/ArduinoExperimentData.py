4b68b118ef70dc69015186ac3cd197164274de51
Christopher Coram <ultraartist15@gmail.com>
2019-06-24 23:58:15 -0400

    Add files via upload

---
+++ Python Charted Data/ArduinoExperimentData.py
@@ -0,0 +1,54 @@
+"""  
+@ Christopher Sean Coram
+June - 23 - 2019
+Gathered Data From Arduino Robotic Arm
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Variables For Class Data One
+N = 5 # Five Weeks In Total Experiment Results Per Week
+improvement_Productivity = [, , , , ,]
+np.apend(N)
+width = 0.25
+# First Class of Data / Improved function and efficiency of span of 5 weeks
+class DataOne():
+def __init__(self, x, y,):
+x = self.x = x
+y = self.y = y
+
+def PlotData(self)
+# Plot Data
+p1 = plt.show(width, improve_Productivity, )
+plt.title("Improved Efficiency Over Time")
+plt.xticks()
+plt.yticks()
+
+result_x = DataOne()
+
+
+# Seconnd Class of Data / Rate of visibility 
+class DataTwo():
+ def PlotDataTwo(self):
+
+result_y = DataTwo()
+
+# Third Class of Data
+class DataThree():
+ def PlotDataThree(self):
+ 
+result_z = DataThree()
+
+# User Input Loop
+x = input("Data_One")
+y = input("Data_Two")
+z = input("Data_Three")
+while True: # While True loop with if and else statement
+# if and else statements
+if x == "Data_One":
+ print(result_x.PlotData)
+else y == "Data_Two":
+ print(result_y.PlotDataTwo)
+else z == "Data_Three"
+ print(result_z.PlotDataThree)
+
